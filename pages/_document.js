52f33c8eaf0af92c759c89b83f8d6bc429e9decc
MohamedFadelMoumeni <jackbadas01@gmail.com>
2022-09-05 21:48:06 +0100

    up

--- pages/_document.js
+++ pages/_document.js
@@ -15,6 +15,21 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400;600;700&display=swap"
             rel="stylesheet"
           />
+          <meta
+            name="description"
+            content="Mohamed Fadel Moumeni ,  a self-taught Full Stack JavaScript Developer, UI/UX Designer,Security Pentester."
+          />
+          <meta
+            name="keywords"
+            content="Mohamed Fadel Moumeni, Develepement informatique, Software engineer, software developer, developer , javascript, mohamed fadel moumeni, moumeni , portfolio, reactjs "
+          />
+          <meta httpEquiv="content-language" content="en" />
+          <meta
+            property="og:description"
+            content="Mohamed Fadel Moumeni ,  a self-taught Full Stack JavaScript Developer, UI/UX Designer,Security Pentester."
+          />
+          <meta property="og:url" content="https://medfadelmoumeni.com" />
+          <meta property="og:type" content="website" />
         </Head>
         <body>
           <Main />
