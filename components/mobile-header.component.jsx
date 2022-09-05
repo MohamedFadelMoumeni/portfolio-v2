52d4d657b42d92a474905416dcdea27fdde04a2c
MohamedFadelMoumeni <jackbadas01@gmail.com>
2022-09-05 21:45:04 +0100

    up

--- components/mobile-header.component.jsx
+++ components/mobile-header.component.jsx
@@ -28,8 +28,8 @@ const Mobileheader = () => {
             onClick={toggle}
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M6 18L18 6M6 6l12 12"
             />
           </svg>
